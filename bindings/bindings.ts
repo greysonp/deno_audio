010699ffd2d4ba8f9a3f19d3a559edf1660020a0
Greyson Parrelli <greyson@signal.org>
2022-11-04 17:32:49 -0400

    Update bindings.

--- bindings/bindings.ts
+++ bindings/bindings.ts
@@ -1,14 +1,17 @@
 // Auto-generated with deno_bindgen
-import { CachePolicy, prepare } from "https://deno.land/x/plug@0.5.1/plug.ts"
+import { CachePolicy, prepare } from "https://deno.land/x/plug@0.5.2/plug.ts"
+
 function encode(v: string | Uint8Array): Uint8Array {
   if (typeof v !== "string") return v
   return new TextEncoder().encode(v)
 }
+
 function decode(v: Uint8Array): string {
   return new TextDecoder().decode(v)
 }
+
 function readPointer(v: any): Uint8Array {
-  const ptr = new Deno.UnsafePointerView(v as Deno.UnsafePointer)
+  const ptr = new Deno.UnsafePointerView(v as bigint)
   const lengthBe = new Uint8Array(4)
   const view = new DataView(lengthBe.buffer)
   ptr.copyInto(lengthBe, 0)
@@ -16,13 +19,30 @@ function readPointer(v: any): Uint8Array {
   ptr.copyInto(buf, 4)
   return buf
 }
+
+const url = new URL("../target/release", import.meta.url)
+let uri = url.toString()
+if (!uri.endsWith("/")) uri += "/"
+
+let darwin: string | { aarch64: string; x86_64: string } = uri
+  + "libdeno_audio.dylib"
+
+if (url.protocol !== "file:") {
+  // Assume that remote assets follow naming scheme
+  // for each macOS artifact.
+  darwin = {
+    aarch64: uri + "libdeno_audio_arm64.dylib",
+    x86_64: uri + "libdeno_audio.dylib",
+  }
+}
+
 const opts = {
   name: "deno_audio",
-  url:
-    (new URL(
-      "https://github.com/littledivy/deno_audio/releases/download/0.2.0",
-      import.meta.url,
-    )).toString(),
+  urls: {
+    darwin,
+    windows: uri + "deno_audio.dll",
+    linux: uri + "libdeno_audio.so",
+  },
   policy: undefined,
 }
 const _lib = await prepare(opts, {
@@ -31,7 +51,8 @@ const _lib = await prepare(opts, {
 
 export function play(a0: string) {
   const a0_buf = encode(a0)
-  let rawResult = _lib.symbols.play(a0_buf, a0_buf.byteLength)
+  const a0_ptr = Deno.UnsafePointer.of(a0_buf)
+  let rawResult = _lib.symbols.play(a0_ptr, a0_buf.byteLength)
   const result = rawResult
   return result
 }
